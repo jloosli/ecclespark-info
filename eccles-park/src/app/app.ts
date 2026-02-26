77b865cd09475a69a9007d5c53564058d64276d4
Jared Loosli <jloosli@gmail.com>
2026-02-26 13:59:09 -0700

    Add header component and update styles
    - Introduced a new Header component with basic template and styles.
    - Updated app module to include the Header component.
    - Enhanced global styles with a new color palette and typography settings.
    - Added analytics tracking ID in angular.json.

--- eccles-park/src/app/app.ts
+++ eccles-park/src/app/app.ts
@@ -13,12 +13,13 @@ export class App {}
 =======
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('eccles-park');
